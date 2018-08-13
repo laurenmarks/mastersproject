72621240a99dca9896118e09c8926b333e02cb98
laurenmarks <37628306+laurenmarks@users.noreply.github.com>
2018-08-13 11:45:22 +0100

    Update and rename alignment.sh to scripts/alignment.sh

--- scripts/alignment.sh
+++ scripts/alignment.sh
@@ -1,4 +1,4 @@
-#using align.sh
+#WGS alignment
 
 novoalignRef=/home/pontikos/d/human_reference_sequence/human_g1k_v37.fasta.k15.s2.novoindex
 ncores=35
@@ -11,7 +11,7 @@ f2=CL100072051_L2_B5GHUMskzRAAAAAAA_2.fq.gz
 # Aligns to b37 of human genome using novoalign and extracts discordant reads with samblaster
 novoalign -c ${ncores} -o SAM "@RG\tID:${code}\tSM:${code}\tLB:${code}\tPL:BGI" --rOQ --hdrhd 3 -H -k -a -o Soft -t 320 -F STDFQ -f $f1 $f2 -d ${novoalignRef} | samblaster -e -d ${code}_disc.sam  | samtools view -Sb - > ${code}.bam
 
-# Sorts and converts discordant read sam file to bam file
+# Sorts and converts discordant read sam file to bam file.  These can be inspected later on using IGV.
 samtools view -Sb ${code}_disc.sam | novosort - -t . -c ${ncores} -m ${memory2}G -i -o ${code}_disc_sorted.bam
 
 # Sorts the WGS bam
